d41d2f1c88065486e9860549f08de33b620bf4c5
mwturner611 <mwturner611@gmail.com>
2020-02-22 18:02:18 -0600

    javaScript variables and setInterval

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-// var timer = document.getElementById('timer');
+// assign variables
+var timer = document.getElementById('timer');
+var question = document.getElementById('question');
+var answer1 = document.getElementById('answer1');
+var answer2 = document.getElementById('answer2');
+var answer3 = document.getElementById('answer3');
+var answer4 = document.getElementById('answer4');
+var button = document.getElementById('button');
+var timeRemaining;
+var numberOfClicks = 0;
 
+// start quiz
+function startQuiz () {
+    timeRemaining = 10;
 
+    setInterval(function() {
+        if (timeRemaining >= 0) {
+          console.log(timeRemaining);
+          timer.innerText = timeRemaining;
+          timeRemaining--;
+        }
+        else{
+            clearInterval();
+        }
+    }, 1000);
+}
 
-
-
-// var timeRemaining = 60;
-
-// // timer function
-// function countDown()
+button.addEventListener("click", function(){
+  if (numberOfClicks === 0){
+    startQuiz();
+  }
+  else{
+    nextQuestion();
+  }
+});
